6e5cf7a3dc119ade0c2f4fdabe9b0e224bce7b10
swhawes <shawes@fiu.edu>
2024-06-20 09:24:44 -0400

    basic download link and iframe tests working

---
+++ docs/components/ToggleCodeBlock.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <button
+      class="toggle-button"
+      @click="toggleCode"
+    >
+      {{ isVisible ? 'Hide Code' : 'Show Code' }}
+    </button>
+    <pre
+      v-if="isVisible"
+      class="code-block"
+    >
+
+      <code><slot /></code>
+    </pre>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isVisible = ref(false)
+function toggleCode() {
+  isVisible.value = !isVisible.value
+}
+</script>
+
+<style scoped>
+.toggle-button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+.code-block {
+  background-color: #f5f5f5;
+  padding: 10px;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+  white-space: pre-wrap;
+}
+</style>
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@nuxt/ui-pro'],
+  components: true,
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
