91e41c7aca2bc54558375fe4151cf32d8049bac9
swhawes <shawes@fiu.edu>
2024-06-21 15:01:24 -0400

    plotly-view and table-view working independently

--- docs/app.vue
+++ docs/app.vue
@@ -35,61 +35,11 @@ useSeoMeta({
 })
 
 provide('navigation', navigation)
-
-const x = [1, 2, 3, 4, 5]
-const y = [10, 20, 30, 20, 10]
-const data: NuxtPlotlyData = [
-  { x: x, y: y, type: 'scatter', mode: 'markers', marker: { size: 20 } }
-]
-const layout: NuxtPlotlyLayout = {
-  title: 'My graph on app.vue with <client-only>'
-}
-
-const config: NuxtPlotlyConfig = { scrollZoom: true, displayModeBar: false }
-
-function myChartOnReady(plotlyHTMLElement: NuxtPlotlyHTMLElement) {
-  const { $plotly } = useNuxtApp()
-  console.log({ $plotly })
-  console.log({ plotlyHTMLElement })
-
-  plotlyHTMLElement.on?.('plotly_afterplot', function () {
-    console.log('done plotting')
-  })
-
-  plotlyHTMLElement.on?.('plotly_click', function () {
-    alert('You clicked this Plotly chart!')
-
-    // use plotly function via `$plotly` to download chart image
-    $plotly.downloadImage(plotlyHTMLElement as HTMLElement, {
-      format: 'png',
-      width: 800,
-      height: 600,
-      filename: 'newplot'
-    })
-  })
-}
 </script>
 
 <template>
-  <div>Longitudinal Data Science</div>
-  <div>
-    <!-- Close the opening <div> tag here -->
-    <h1>nuxt-plotly module playground!</h1>
-    <client-only>
-      <nuxt-plotly
-        :data="data"
-        :layout="layout"
-        :config="config"
-        style="width: 100%"
-        @on-ready="myChartOnReady"
-      />
-    </client-only>
-    <pie-chart />
-  </div>
   <NuxtLoadingIndicator />
-
   <AppHeader />
-
   <UMain>
     <NuxtLayout>
       <NuxtPage />
--- docs/pages/table-view.vue
+++ docs/pages/table-view.vue
@@ -1,65 +1,14 @@
-<!-- pages/table-view.vue -->
 <template>
   <div>
+    <!-- Start of the single root element -->
     <h1>Table View</h1>
     <UTable :rows="people" />
   </div>
-  
-    <!-- Close the opening <div> tag here -->
-    <h1>nuxt-plotly module playground!</h1>
-    <client-only>
-      <nuxt-plotly
-        :data="data"
-        :layout="layout"
-        :config="config"
-        style="width: 100%"
-        @on-ready="myChartOnReady"
-      />
-    </client-only>
-    <pie-chart />
-
+  <!-- End of the single root element -->
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import type {
-  NuxtPlotlyConfig,
-  NuxtPlotlyData,
-  NuxtPlotlyLayout,
-  NuxtPlotlyHTMLElement
-} from 'nuxt-plotly'
-import { people } from '~/data/peopleData'
+// import { ref } from "vue";
 
-const x = [1, 2, 3, 4, 5]
-const y = [10, 20, 30, 20, 10]
-const data: NuxtPlotlyData = [
-  { x: x, y: y, type: 'scatter', mode: 'markers', marker: { size: 20 } }
-]
-const layout: NuxtPlotlyLayout = {
-  title: 'My graph on app.vue with <client-only>'
-}
-
-const config: NuxtPlotlyConfig = { scrollZoom: true, displayModeBar: false }
-
-function myChartOnReady(plotlyHTMLElement: NuxtPlotlyHTMLElement) {
-  const { $plotly } = useNuxtApp()
-  console.log({ $plotly })
-  console.log({ plotlyHTMLElement })
-
-  plotlyHTMLElement.on?.('plotly_afterplot', function () {
-    console.log('done plotting')
-  })
-
-  plotlyHTMLElement.on?.('plotly_click', function () {
-    alert('You clicked this Plotly chart!')
-
-    // use plotly function via `$plotly` to download chart image
-    $plotly.downloadImage(plotlyHTMLElement as HTMLElement, {
-      format: 'png',
-      width: 800,
-      height: 600,
-      filename: 'newplot'
-    })
-  })
-}
+import { people } from "~/data/peopleData";
 </script>
