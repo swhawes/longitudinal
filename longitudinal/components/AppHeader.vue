f40f532b33e86a3e17cf8fe93e911628a7f2808f
swhawes <shawes@fiu.edu>
2024-09-12 15:39:59 -0400

    updated lmm tutorial

--- longitudinal/components/AppHeader.vue
+++ longitudinal/components/AppHeader.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
-import type { NavItem } from "@nuxt/content/dist/runtime/types";
-import AppSubheader from "./AppSubheader.vue"; // Import the new Subheader component
+import type { NavItem } from '@nuxt/content/dist/runtime/types'
+import AppSubheader from './AppSubheader.vue' // Import the new Subheader component
 
-const navigation = inject<NavItem[]>("navigation", []);
+const navigation = inject<NavItem[]>('navigation', [])
 
-const { header } = useAppConfig();
+const { header } = useAppConfig()
 </script>
 
 <template>
   <UHeader>
     <template #logo>
       <template v-if="header?.logo?.dark || header?.logo?.light">
-        <UColorModeImage v-bind="{ class: 'h-6 w-auto', ...header?.logo }" />
+        <!-- <UColorModeImage v-bind="{ class: 'h-6 w-auto', ...header?.logo }" /> -->
       </template>
       <template v-else>
         <span class="highlighted-logo">Longitudinal.dev</span>
         <!-- Updated UBadge with a router-link to the /about page -->
         <router-link to="/about">
-          <UBadge label="Docs" variant="subtle" class="mb-0.5 cursor-pointer" />
+          <UBadge
+            label="Docs"
+            variant="subtle"
+            class="mb-0.5 cursor-pointer"
+          />
         </router-link>
       </template>
     </template>
 
-    <template v-if="header?.search" #center>
+    <template
+      v-if="header?.search"
+      #center
+    >
       <UContentSearchButton class="hidden lg:flex" />
     </template>
 
